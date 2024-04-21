accc1209396fabbb2269d2d6e83f53fbd8757088
Andrii Petrovskyi <angpysha@gmail.com>
2024-04-21 17:36:55 +0300

    Clean up

--- packages/firebase_core/firebase_core/windows/include/firebase_core/firebase_plugin_registry.h
+++ packages/firebase_core/firebase_core/windows/include/firebase_core/firebase_plugin_registry.h
@@ -26,45 +26,22 @@ namespace firebase_core_windows {
 
     class FirebasePluginRegistry {
     public:
-         FirebasePluginRegistry ()
-         {
-             srand(time(NULL));
-             value_ = rand() % 150;
-             test_strings_ = {};
-             pConstants_ = {};
-             test_strings_.push_back("testt");
-         }
 
-         static FirebasePluginRegistry* GetInstance();
-//        static FirebasePluginRegistry* GetInstance() {
-//            static FirebasePluginRegistry instance;
-//            return &instance;
-//        }
-//
-//        static FirebasePluginRegistry* GetSharedInstance();
-//
-//        FirebasePluginRegistry(const FirebasePluginRegistry&) = delete;
-//        FirebasePluginRegistry& operator=(const FirebasePluginRegistry&) = delete;
+        static FirebasePluginRegistry *GetInstance();
 
-        void RegisterPlugin(std::string pluginName, std::string version, FlutterFirebasePlugin* plugin);
+        void put_plugin_ref(std::shared_ptr <FlutterFirebasePlugin>);
 
-        std::map<std::string, FlutterFirebasePlugin*>& pluginConstants();
-        void put_plugin_ref(std::shared_ptr<FlutterFirebasePlugin>);
-        std::vector<std::shared_ptr<FlutterFirebasePlugin>>& p_constants();
+        std::vector <std::shared_ptr<FlutterFirebasePlugin>> &p_constants();
+
         std::string app_name;
 
     private:
-//        FirebasePluginRegistry() = default;
-//        mutable std::mutex mutex_;
-        std::map<std::string, FlutterFirebasePlugin*> pluginConstants_;
-        int value_;
-        std::vector<std::string> test_strings_;
-        std::vector<std::shared_ptr<FlutterFirebasePlugin>> pConstants_;
+        FirebasePluginRegistry() {
+            pConstants_ = {};
+        }
+        std::vector <std::shared_ptr<FlutterFirebasePlugin>> pConstants_;
+        friend class FirebaseCorePlugin;
     };
 
 }
 #endif //TODO_POINTS_FIREBASE_PLUGIN_REGISTRY_H
-
-//MY_DLL_API ::firebase_core_windows::FirebasePluginRegistry& get_flutter_firebase_plugin_registry();
-
-//MY_DLL_API void add_plugin_constant(std::shared_ptr<::firebase_core_windows::FlutterFirebasePlugin> item);
